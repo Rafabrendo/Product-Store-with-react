b6109222d8f92faf06835d5c355d37fb4256d496
Rafael <rafalive122@hotmail.com>
2024-11-07 03:43:22 -0300

    Update

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,32 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req,res) => {
+app.post("/products", async (req,res) => {
     // res.send("Server is ready");
+    const product = req.body; // user will send this data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({
+            success:false,
+            message: "Please provide all fields"
+        })
+    }
+    const newProduct = new Product(product)
     
+    try {
+        await newProduct.save();
+        res.status(201).json({
+            success:true,
+            data: newProduct
+        })
+    } catch (error) {
+        console.error("Error in Create product:", error.message)
+        res.status(500).json({
+            success:false,
+            message: "Server Error"
+        })
+    }
+
 });
 
 // console.log(process.env.MONGO_URI);
