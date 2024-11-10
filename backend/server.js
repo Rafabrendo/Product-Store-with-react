f3c8260372c3b19587578e450b73c5d6f62c9e1f
Rafael <rafalive122@hotmail.com>
2024-11-09 23:58:02 -0300

    post

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
+
 const app = express();
 
-app.post("/products", async (req,res) => {
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.post("/api/products", async (req, res) => {
     // res.send("Server is ready");
     const product = req.body; // user will send this data
 
@@ -39,5 +43,5 @@ app.post("/products", async (req,res) => {
 
 app.listen(5000, () => {
     connectDB();
-    console.log('Server started at hhtp://localhost:5000');
+    console.log('Server started at http://localhost:5000');
 });
