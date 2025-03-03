2af749c50567f15824aceb7b3b7f5f41e85c3455
Rafael <rafalive122@hotmail.com>
2025-03-02 23:24:47 -0300

    start home page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Container, Heading, VStack, Box, useColorModeValue, Input, Button } from "@chakra-ui/react";
+import { Container, Heading, VStack, Box, useColorModeValue, Input, Button, useToast } from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
 
 
 
@@ -10,20 +11,49 @@ const CreatePage = () =>{
         image: "",
     });
 
-    const handleAddProduct = () =>{
-        console.log(newProduct);
+    const {createProduct} = useProductStore()
+    const toast = useToast()
+
+    const handleAddProduct = async () => {
+		const { success, message } = await createProduct(newProduct);
+        console.log("Success:", success);
+        console.log("Message:",message);
+        if(!success){
+            toast({
+                title:"Error",
+                description:message,
+                status:"error",
+                // duration: 5000
+                isClosable: true
+            })
+        }else{
+            toast({
+                title:"Success",
+                description: message,
+                status: "success",
+                isClosable: true
+            })
+        }
+        
+        setNewProdutc({name: "", price:"", image: ""})
+
+
+
     }
 
     return <Container maxW={"container.sm"}>
+                
                 <VStack
-                    spacing={8}
-                ></VStack>
+                    spacing={17}
+                >
                 <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={12}>
                     Create New Product
                 </Heading>
+                
                 <Box
                    w={"full"} bg={useColorModeValue("white", "#21277beb" )}
                    p={6} rounded={"lg"} shadow={"md"}
+                    
                 >
                     <VStack spacing={5}>
                     <Input 
@@ -56,6 +86,7 @@ const CreatePage = () =>{
 
                     </VStack>
                 </Box>
+                </VStack>
             </Container>
     
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,25 @@
+import { Container, VStack, Text, Box} from "@chakra-ui/react";
+import { RiSpaceShipLine } from "react-icons/ri";
+
 const HomePage = () => {
-    return <div>HomePage</div>
+    return (
+        <Container maxW={"container.x1"} py={12}>
+            <VStack spacing={8}>
+            <Box display={"flex"} gap={1}>
+                <Text
+                    fontSize={"30px"}
+                    fontWeight={"bold"}
+                    bgGradient={"linear(to-r, cyan.400, blue.500)"}
+                    bgClip={"text"}
+                    textAlign={"center"}
+                
+                >
+                    Current Products
+                </Text><RiSpaceShipLine size={40} />
+            </Box>
+
+            </VStack>
+        </Container>
+        )
 };
 export default HomePage;
